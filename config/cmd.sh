54a959a2f82bf8284654fb2ce7e719a3ab11edca
Mauricio Nunes <mutila@gmail.com>
2019-11-18 13:58:00 -0300

    Update cmd.sh

--- config/cmd.sh
+++ config/cmd.sh
@@ -12,6 +12,9 @@ sed -i -e "s/display_errors =.*/display_errors = Off/g" /etc/php/5.6/fpm/php.ini
 procs=$(cat /proc/cpuinfo |grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
+echo "deb http://ppa.launchpad.net/certbot/certbot/ubuntu trusty main" >> /etc/apt/sources.list
+apt update
+
 # Set the root in the conf
 sed -i -e "s#%%NGINX_ROOT%%#$NGINX_ROOT#" /etc/nginx/sites-available/default.conf
 
